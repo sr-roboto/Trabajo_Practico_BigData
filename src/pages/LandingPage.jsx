34cae1770454f617635063d87ad85c8bb748a02f
sr-roboto <marcoslopez123.lu@gmail.com>
2025-04-22 06:56:04 -0300

    Initialize project structure with essential files and components
    - Added .keep files in assets and components directories to ensure they are tracked by Git.
    - Created index.css for global styles.
    - Set up main.jsx as the entry point for the React application, integrating React Router.
    - Developed LandingPage component with a basic structure and D3.js imported for future use.
    - Configured Vite for the React project with the necessary plugin.

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as d3 from 'd3';
+
+function LandingPage() {
+  return (
+    <div>
+      <div>holaaa</div>
+    </div>
+  );
+}
+
+export default LandingPage;
