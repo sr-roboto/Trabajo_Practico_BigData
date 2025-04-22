5ad695396704f8f1d65edc06acd61258579b2af4
sr-roboto <marcoslopez123.lu@gmail.com>
2025-04-22 11:47:50 -0300

    feat: integrate Tailwind CSS and D3.js for enhanced styling and data visualization
    - Added Tailwind CSS and its Vite plugin to the project dependencies.
    - Imported Tailwind CSS in the main CSS file.
    - Updated LandingPage component to load and log CSV data using D3.js.
    - Configured Vite to include Tailwind CSS plugin and allow CSV files as assets.

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import * as d3 from 'd3';
+import primer from '../assets/csv/1- share-of-the-world-population-with-at-least-basic-education.csv';
 
 function LandingPage() {
+  d3.csv(primer).then((data) => {
+    console.log(data);
+  });
   return (
     <div>
       <div>holaaa</div>
